9caee524f75935e86eaca64528c1212467bec100
M. Kh. Umam <www.umam@gmail.com>
2022-04-04 10:22:12 +0700

    Minor update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
 import './index.css';
-
-class Main extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <App />
-      </React.Fragment>
-    );
-  }
-}
 
 ReactDOM.render(
     <React.StrictMode>
-        <Main />
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
